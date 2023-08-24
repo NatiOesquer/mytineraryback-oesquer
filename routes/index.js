e8e8c4c6eb9331e2ccd4fe2450e53f5beeb4573f
NatiOesquer <94532066+NatiOesquer@users.noreply.github.com>
2023-08-24 19:43:37 -0300

    itineraries y activities (#7)

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import userRouter from './users.js'
 import citiRouter from './cities.js'
+import itinerariesRouter from './itineraries.js'
+import activitiesRouter from './activities.js'
 
 
 let router = express.Router();
@@ -13,5 +15,7 @@ router.get('/', function(req, res, next) {
 
 router.use('/users',userRouter)
 router.use('/cities',citiRouter)
+router.use('/itineraries',itinerariesRouter)
+router.use('/activities',activitiesRouter)
 
 export default router;
