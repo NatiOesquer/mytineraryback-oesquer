68550a1e31630b767bea33e357e3cafb642cda8b
NatiOesquer <nati.oesquer@hotmail.com>
2023-08-30 11:10:41 -0300

    itineraries

--- controllers/itineraries/update.js
+++ controllers/itineraries/update.js
@@ -1,22 +1,27 @@
 import Itinerary from "../../models/Itinerary.js";
 
-export default async (req, res) => {
-    try {
-      let updatedItinerary = await Itinerary.findByIdAndUpdate(
-        req.params.u_id,
-        req.body,
-        { new: true }
-      ).select("name price photo");
+export default async (req, res, next) => {
+  try {
+    let updatedItinerary = await Itinerary.findByIdAndUpdate(
+      req.params.u_id,
+      req.body,
+      { new: true }
+    ).select("name price photo");
+    if (updatedItinerary) {
       return res.status(200).json({
         success: true,
         message: "Itinerary updated!",
         response: updatedItinerary,
       });
-    } catch (error) {
-      return res.status(400).json({
+    } else {
+      return res.status(404).json({
         success: false,
-        message: "Itinerary not updated",
-        response: null,
-      });
+        message: 'itinerary not found',
+        response: null
+      })
     }
-  };
+
+  } catch (error) {
+    next(error)
+  }
+};
--- models/Itinerary.js
+++ models/Itinerary.js
@@ -6,11 +6,14 @@ let schema = new Schema({
     name: { type:String,require:true },
     city_id: { type:Types.ObjectId,require:true,ref:'cities' },
     price: { type:Number, require:true },
-    duration: { type:String },
-    tags: { type:Array},
+    duration: { type:Number,require:true },
+    tags: [{ type:String,require:true}],
     photo: { type:String,require:true}
 
-})
+},{
+    timestamps: true
+}
+)
 
 let Itinerary = model(collection,schema)
 export default Itinerary
