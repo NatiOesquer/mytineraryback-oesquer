3291e98105a2bce549b1b523d8c4ffeadbed8b67
NatiOesquer <94532066+NatiOesquer@users.noreply.github.com>
2023-09-06 22:22:49 -0300

    loginjwt (#12)

--- routes/auth.js
+++ routes/auth.js
@@ -4,11 +4,17 @@ import isValidPass from "../middlewares/isValidPass.js"
 import existsUser from "../middlewares/existsUser.js"
 import validator from "../middlewares/validator.js"
 import registerSchema from "../schemas/register.js"
+import signin from "../controllers/auth/signin.js"
+import signinSchema from "../schemas/signin.js"
+import notExistsUser from "../middlewares/notExistsUser.js"
+import isPassOk from "../middlewares/isPassOk.js"
+import isValidToken from "../middlewares/isValidToken.js"
 
 
 let authRouter = Router()
 
 authRouter.post('/register',validator(registerSchema),existsUser,isValidPass,register)
+authRouter.post('/signin',validator(signinSchema),notExistsUser,isPassOk,isValidToken,signin)
 
 
 export default authRouter
