dd22344bbf0a724be99b13676de2a7f3f37f3b59
NatiOesquer <nati.oesquer@hotmail.com>
2023-09-12 11:52:53 -0300

    sweet

--- schemas/register.js
+++ schemas/register.js
@@ -7,7 +7,7 @@ let registerSchema = joi.object({
         "any.required": "name is required", //para cuando NO se envía el dato
         "string.empty": "name is required"  //para cuando se envía VACÍO
     }), 
-       mail: joi.string().required().min(10).max(30).messages({
+       mail: joi.string().required().email().min(10).max(30).messages({
         'string.min': "mail must have at least 10 characters please!",
         "string.max": "mail must be less than 31 characters please!",
         "any.required": "mail is required", //para cuando NO se envía el dato
@@ -29,9 +29,9 @@ let registerSchema = joi.object({
         'string.min': "last name must have at least 3 characters please!",
         "string.max": "last name must be less than 21 characters please!",
     }),
-       photo: joi.string().min(3).max(30).empty("").messages({
-        'string.min': "last name must have at least 3 characters please!",
-        "string.max": "last name must be less than 21 characters please!",
+       photo: joi.string().min(3).max(300).empty("").messages({
+        'string.min': "photo must have at least 3 characters please!",
+        "string.max": "photo must be less than 301 characters please!",
     })
 })
 
